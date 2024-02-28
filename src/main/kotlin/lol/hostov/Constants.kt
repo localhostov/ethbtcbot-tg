b8808139bc98710ff91d8d24133a2b968a905377
Alexander Localhostov <localhostov@gmail.com>
2024-02-28 21:40:15 +0300

    feat: Add Constants file

---
+++ src/main/kotlin/lol/hostov/Constants.kt
@@ -0,0 +1,10 @@
+package lol.hostov
+
+object Constants {
+    const val OWNER_TG_ID = 0L // https://t.me/getmyid_bot
+    const val TG_CHAT_ID = 0 // https://t.me/get_id_channel_bot
+    const val TG_BOT_TOKEN = "" // https://t.me/BotFather
+    const val COINSTATS_API_KEY = "" // https://openapi.coinstats.app/
+    const val COINSTATS_API_URL = "https://openapiv1.coinstats.app/coins?limit=2"
+    const val BOT_AVATAR_URL = "https://i.ibb.co/sV5zf0h/Frame-26.png"
+}
